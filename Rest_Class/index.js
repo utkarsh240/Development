b1c47e47286ebee51bfbe94f1ff50b411713b903
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2024-07-22 23:29:03 +0530

    .

--- Rest_Class/index.js
+++ Rest_Class/index.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+
 
 app.use(express.urlencoded({extended: true}));
 
@@ -12,14 +14,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts =[
     {
+        id: uuidv4(),
         username : "apnacollege",
         content : " i love coding",
     },
     {
+        id : uuidv4(),
         username : " utkarsh",
         content : "i work hard",
     },
     {
+        id :uuidv4(),
         username : " akanshaw",
         content :" my brother",
 
@@ -34,10 +39,34 @@ app.get("/posts/new",(req,res)=>{
 })
 
 app.post("/posts",(req,res)=>{
-    console.log(req.body);
-    res.send("working")
+    let {username,content}= req.body;
+    let id = uuidv4();
+
+    posts.push({id, username,content});
+    res.redirect("/posts");
 })
 
+app.get("/posts/:id",(req,res)=>{
+    let {id}= req.params;
+    let post =posts.find((p)=>id=p.id);
+    res.render("show.ejs",{post})
+
+});
+
+
+app.patch("/posts/:id",(req,res)=>{
+    let{id}= req.params;
+    let newContent = req.body.content;
+    let post =posts.find((p)=>id=p.id);
+    post.content = newContent;
+    console.log(post);
+    res.send("patch request working");
+})
+
+app.get("/posts/:id/edit",({req,res})=>{
+    let {id} = req.params;
+    let post = posts.find((p)=>id=p.id);
+})
 app.listen(port, () => {
     console.log("listening to port: 8080");
 });
